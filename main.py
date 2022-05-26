0392ebe14f8eb366867bc815c3d2a1af12bfe512
hexsix <hexsixology@gmail.com>
2022-05-26 18:51:41 +0800

    v1.0

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ description:
 import json
 import logging
 import os
+import re
 import time
 from typing import Any, Dict, List
 
+from bs4 import BeautifulSoup
 import feedparser
 import httpx
+import redis
 
 
 logging.basicConfig(
@@ -24,6 +27,8 @@ logger = logging.getLogger()
 
 RSS_URL = os.environ['RSS_URL']
 TG_TOKEN = os.environ['TG_TOKEN']
+REDIS = redis.from_url(os.environ['REDIS_URL'])
+CONFIGS = json.loads(open('configs.json', 'r', encoding='utf8'))
 
 
 def download() -> Dict:
@@ -49,45 +54,152 @@ def download() -> Dict:
 def parse(rss_json: Dict) -> List[Dict[str, Any]]:
     logger.info('Parsing RSS ...')
     items = []
+    filtered_cnt = 0
     for entry in rss_json['entries']:
         try:
             item = dict()
-
+            if 'type_SOU' not in entry['summary']:
+                filtered_cnt += 1
+                continue
+            soup = BeautifulSoup(entry['summary'], 'html.parser')
+            """work_category
+            <div class="work_category type_SOU">
+                <a href="https://www.dlsite.com/maniax/fsr/=/work_type/SOU" target="_blank">
+                    ボイス・ASMR
+                </a>
+            </div>
+            """
+            work_category = 'ボイス・ASMR'
+            """img
+            <img alt="オナサポ科のJK双子ママ♪～焦らしテクで快楽絶頂!～ [犬走り]" class="lazy" src="https://img.dlsite.jp/resize/images2/work/doujin/RJ392000/RJ391233_img_main_240x240.jpg"/>
+            """
+            # try:
+            #     item['img_url'] = soup.select_one('img').attrs['src']
+            # except KeyError:
+            #     item['img_url'] = ''
+            """work_name
+            <dt class="work_name">
+                <span class="period_date">
+                    2022年06月22日 23時59分 割引終了
+                </span>
+                <div class="icon_wrap">
+                    <span class="icon_lead_01 type_exclusive" title="DLsite専売">
+                        DLsite専売
+                    </span>
+                </div>
+                <a href="https://www.dlsite.com/maniax/work/=/product_id/RJ391233.html" target="_blank" title="オナサポ科のJK双子ママ♪～焦らしテクで快楽絶頂!～">
+                    オナサポ科のJK双子ママ♪～焦らしテクで快楽絶頂!～
+                </a>
+            </dt>
+            """
+            item['work_name'] = soup.select_one(
+                '.work_name').select_one('a').attrs['title']
+            item['author'] = entry['author']
+            item['tags'] = [t['term'] for t in entry['tags']]
+            item['link'] = entry['link']
+            item['rj_code'] = re.search(r'RJ\d*', entry['link']).group()
             items.append(item)
         except Exception as e:
             logger.info(f'Exception: {e}')
             continue
     logger.info(
-        f"Parse RSS End. {len(items)}/{len(rss_json['entries'])} Succeed.\n")
+        f"Parse RSS End. {len(items)}/{len(rss_json['entries'])} Succeed. {filtered_cnt} filtered by ASMR. Others failed.")
     return items
 
 
 def send(chat_id: str, photo: str, caption: str, rj_code: str) -> bool:
     logger.info(f'Send rj code: {rj_code} ...')
-    target = f"https://api.telegram.org/bot{TG_TOKEN}/sendPhoto"
-    params = {
-        'chat_id': chat_id,
-        'photo': photo,
-        'caption': caption
-    }
-    try:
-        with httpx.Client() as client:
-            response = client.post(target, params=params)
-        if response.json()['ok']:
-            logger.info(f'Succeed to send {rj_code}.')
-            return True
-        else:
-            logger.warn(f'Telegram api returns {response.json()}')
-            logger.warn(f'photo: {photo}, caption: {caption}')
-    except Exception as e:
-        logger.error(f'Exception: {e}')
-        pass
-    logger.error(f'Failed to send {rj_code}.\n')
+    if photo:
+        target = f"https://api.telegram.org/bot{TG_TOKEN}/sendPhoto"
+        params = {
+            'chat_id': chat_id,
+            'photo': photo,
+            'caption': caption
+        }
+        try:
+            with httpx.Client() as client:
+                response = client.post(target, params=params)
+            if response.json()['ok']:
+                logger.info(f'Succeed to send {rj_code}.')
+                return True
+            else:
+                logger.warn(f'Telegram api returns {response.json()}')
+                logger.warn(f'photo: {photo}, caption: {caption}')
+        except Exception as e:
+            logger.error(f'Exception: {e}')
+            pass
+        logger.error(f'Failed to send {rj_code}.\n')
+        return False
+    else:
+        target = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+        params = {
+            'chat_id': chat_id,
+            'text': caption
+        }
+        try:
+            with httpx.Client() as client:
+                response = client.post(target, params=params)
+            if response.json()['ok']:
+                logger.info(f'Succeed to send {rj_code}.')
+                return True
+            else:
+                logger.warn(f'Telegram api returns {response.json()}')
+                logger.warn(f'photo: {photo}, caption: {caption}')
+        except Exception as e:
+            logger.error(f'Exception: {e}')
+            pass
+        logger.error(f'Failed to send {rj_code}.\n')
+        return False
+
+
+def construct_params(item: Dict):
+    rj_code = item['rj_code']
+    photo = ''
+    caption = f'#{rj_code}\n' \
+              f'{item["work_name"]}\n' \
+              f'author: {item["author"]}\n' \
+              f'tags: {" ".join("#" + t for t in item["tags"])}\n' \
+              f'\n' \
+              f'{item["link"]}'
+    return photo, caption, rj_code
+
+
+def filter(item: Dict) -> bool:
+    if REDIS.exists(item['rj_code']):
+        return True
     return False
 
 
-if __name__ == '__main__':
+def redis_set(rj_code: str) -> bool:
+    for retry in range(5):
+        logger.info(f'The {retry + 1}th attempt to set redis, 5 attempts in total.')
+        try:
+            if REDIS.set(rj_code, 'sent', ex=2678400):  # expire after a month
+                logger.info(f'Succeed to set redis {rj_code}.\n')
+                return True
+        except Exception:
+            logger.info('Failed to set redis, '
+                        'the next attempt will start in 6 seconds.')
+            time.sleep(6)
+    logger.info(f'Failed to set redis, {rj_code} may be sent twice.\n')
+    return False
+
+
+def main() -> None:
+    logger.info('============ App Start ============')
     rss_json = download()
-    json.dump(rss_json, open("sample.json", 'w', encoding='utf8'),
-              ensure_ascii=False, indent=4)
-    logging.info(str(rss_json))
+    items = parse(rss_json)
+    filtered_items = [item for item in items if not filter(item)]
+    logger.info(f'{len(filtered_items)}/{len(items)} filtered by already sent.\n')
+    for item in filtered_items:
+        photo, caption, rj_code = construct_params(item)
+        for rss_author in CONFIGS.keys:
+            if rss_author in item['author']:
+                send(CONFIGS[rss_author], photo, caption, rj_code)
+                redis_set(rj_code)
+                time.sleep(10)
+    logger.info('============ App End ============')
+
+
+if __name__ == '__main__':
+    main()
